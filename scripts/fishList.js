736b2bcc418c84bbe780b6ec36bc945aa3edc5b0
Eric Martinez <ericmartinez1007@gmail.com>
2026-02-17 14:44:27 -0600

    Added fishHTML and renderFishList code

---
+++ scripts/fishList.js
@@ -0,0 +1,31 @@
+import { database } from './aquariumData.js';
+
+export const fishList = () => {
+    // Generate an HTML representation of each fish
+    let fishHTML = '';
+    for (const fish of database.fish) {
+        fishHTML += `
+            <article class="fish-card">
+                <img src="${fish.image}" alt="${fish.name}">
+                <div class="fish-details">
+                    <h2>${fish.name}</h2>
+                    <p>Species: ${fish.species}</p>
+                    <p>Length: ${fish.length} inches</p>
+                    <p>Location: ${fish.location}</p>
+                    <p>Diet: ${fish.diet}</p>
+                </div>
+            </article>
+`;
+    }
+    return fishHTML;
+};
+
+export const renderFishList = (fishHTML) => {
+    const fishList = document.getElementById('fishList');
+
+    if (fishList) {
+        fishList.innerHTML = fishHTML;
+    } else {
+        console.error('Element with id "fishList" not found in the DOM.');
+    }
+};
---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+import { fishList, renderFishList } from './fishList.js';
+//import { tipList } from './tipList.js';
+//import { locationList } from './locationList.js';
+
+// Generate the fish list
+const fishHTML = fishList();
+renderFishList(fishHTML);
+// Generate the care tips
+const tipHTML = tipList();
+
+// Generate the location list
+const locationHTML = locationList();
+
+// Render each HTML string to the correct DOM element
